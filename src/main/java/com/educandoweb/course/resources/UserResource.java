9c3297c2594dcc9f307587fa9944e834f8abcb23
Luiz Gustavo <sivaluiz9@gmail.com>
2024-08-23 08:21:23 -0300

    JPA repository, dependency injection, database seeding(Repositório JPA, injeção de dependência, propagação de banco de dados)

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserResource {
 
     @GetMapping
-    public ResponseEntity<User> findAll(){
+    public ResponseEntity<User> findAll() {
         User u = new User(1L, "Maria", "maria@gmail.com", "9999999", "12345");
         return ResponseEntity.ok().body(u);
     }
